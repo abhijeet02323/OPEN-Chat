db0aeb1dc25be571fc03168134f5d30e29d15360
abhijeet02323 <dwivedi.abhijeet1301@gmail.com>
2026-08-15 19:14:53 +0530

    room id add in table

--- serverless-chat/functions/connect/app.py
+++ serverless-chat/functions/connect/app.py
@@ -1,4 +1,5 @@
 import os
+
 import boto3
 
 
@@ -13,13 +14,24 @@ def lambda_handler(event, context):
 
     connection_id = event["requestContext"]["connectionId"]
 
+    query_parameters = event.get("queryStringParameters") or {}
+
+    room_id = query_parameters.get("roomId", "general")
+
+    if not room_id:
+        room_id = "general"
+
     connections_table.put_item(
         Item={
+            "roomId": room_id,
             "connectionId": connection_id
         }
     )
 
-    print(f"WebSocket connected: {connection_id}")
+    print(
+        f"Connected: {connection_id} "
+        f"to room: {room_id}"
+    )
 
     return {
         "statusCode": 200
